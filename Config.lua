19036722bfb3a723a48092d4ae6226d79bf602df
Nolfi <68621653+Nolfi24@users.noreply.github.com>
2021-03-04 09:16:06 +0100

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,8 @@
+Config = {}
+Config.Buttons = {
+    {index = 0,name = 'Discord',url = 'https://discord.gg/YtKpN4knnU'},
+    {index = 1,name = 'Connect',url = 'fivem://connect/94.130.9.144:30120'}
+}
+
+Config.RichAppId = "597239405593362443"
+Config.RichAssetId = "logo"
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Nolfi'
+
+description 'Discord rich presence'
+
+shared_script 'Config.lua'
+client_script 'Client.lua'
